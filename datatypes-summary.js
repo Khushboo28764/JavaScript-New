d72eb66ae29712f8d8ccab303a32343052a4d850
Khushboo28764 <101248193+Khushboo28764@users.noreply.github.com>
2025-01-02 16:30:13 +0000

    datatypesummary

---
+++ datatypes-summary.js
@@ -0,0 +1,23 @@
+//  Primitive
+//  7 types : String, Number, Boolean, null, undefined,Symbol,
+BigInt
+const score = 100
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id===anotherid)
+
+// Reference (Non primitive)
+// Array, Objects, Functions
+const hero=["shaktiman","naagraj","doga"]
+let myObj={
+    name:"hitesh",
+    age:22,
+}
+// bracket ke andar wala part pura object hai
+const myFunction=function(){
+    console.log("Hello world")
+}
