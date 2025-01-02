d41cd98ee52c9d4cca2edb1ba1413c08632968e8
Khushboo28764 <101248193+Khushboo28764@users.noreply.github.com>
2025-01-02 09:50:25 +0000

    conversion

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let accountEmail="khushbookri6782@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+accountEmail="khushbookumari6853@gmail.com"
+console.log(accountId);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity])
