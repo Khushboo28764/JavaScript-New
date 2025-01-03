6c994f838ed9353c29b19c651d1eaa116a74bf2d
Khushboo28764 <101248193+Khushboo28764@users.noreply.github.com>
2025-01-03 15:35:00 +0000

    done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,10 @@
+// array
+const myArr=[0,1,2,3,4,5, true,"hitesh"]
+const myHeros=["shaktiman","naagraj"]
+const myArr2= new Array(1,2,3,4)
+console.log(myArr[0])
+// Array methods
+myArr.push(6)
+console.log(myArr)
+myArr.unshift(9)
+console.log(myArr)
